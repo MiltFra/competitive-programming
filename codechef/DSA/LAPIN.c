893117ff764e98700d4032265f172322b77c195c
Franz Miltz <franz@miltz.me>
2020-06-22 18:40:23 +0200

    Codechef: DSA Series 1.3 (LAPIN)

---
+++ codechef/DSA/LAPIN.c
@@ -0,0 +1,87 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#define u32_t unsigned int
+#define u64_t unsigned long int
+#define i32_t int
+#define i64_t long int
+
+#define int i32_t
+#define uint u32_t
+
+void next_signed(int *number) {
+  register int c;
+  *number = 0;
+  do {
+    c = getchar();
+  } while (c == '\n' || c == '\r' || c == ' ');
+
+  char negative = c == '-';
+
+  for (; (c > 47 && c < 58); c = getchar())
+    *number = *number * 10 + c - 48;
+  if (negative) {
+    *number *= -1;
+  }
+}
+
+void next_unsigned(uint *number) {
+  register int c;
+  *number = 0;
+  do {
+    c = getchar();
+  } while (c == '\n' || c == '\r' || c == ' ');
+  for (; (c > 47 && c < 58); c = getchar())
+    *number = *number * 10 + c - 48;
+}
+
+void next_lower_letter(char *c) {
+  do {
+    *c = getchar();
+  } while (*c < 97 || *c > 122);
+}
+
+void next_upper_letter(char *c) {
+  do {
+    *c = getchar();
+  } while (*c < 65 || *c > 90);
+}
+
+#define OCCURRENCES_LEN 256
+
+void is_lapindrome(char *buffer) {
+  uint occurences_left[OCCURRENCES_LEN], occurences_right[OCCURRENCES_LEN];
+  char *left, *right;
+  uint len = strlen(buffer);
+  uint part_len = len / 2;
+  for (uint i = 0; i < OCCURRENCES_LEN; i++) {
+    occurences_left[i] = 0;
+    occurences_right[i] = 0;
+  }
+  left = buffer;
+  right = left + (len + 1) / 2;
+  for (uint i = 0; i < part_len; i++) {
+    occurences_left[*(left++)]++;
+    occurences_right[*(right++)]++;
+  }
+  for (uint i = 0; i < OCCURRENCES_LEN; i++) {
+    if (occurences_left[i] != occurences_right[i]) {
+      printf("NO\n");
+      return;
+    }
+  }
+  printf("YES\n");
+}
+
+
+int main(void) {
+  uint T;
+  next_unsigned(&T);
+  char buffer[1001];
+  while (T--) {
+    scanf("%s", &buffer);
+    is_lapindrome(buffer);
+  }
+  return 0;
+}
