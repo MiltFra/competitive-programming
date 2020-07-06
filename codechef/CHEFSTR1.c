87092cd3d44565c98ba425c543e616d62c1f6472
Franz Miltz <franz@miltz.me>
2020-07-06 20:46:51 +0200

    July Long Challenge: Task 1 completed (CHEFSTR1)

---
+++ codechef/CHEFSTR1.c
@@ -0,0 +1,66 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#define u32_t unsigned int
+#define u64_t unsigned long int
+#define i32_t int
+#define i64_t long int
+
+#define int_t i64_t
+#define uint_t u64_t
+
+void next_signed(int_t *number) {
+  register int_t c;
+  *number = 0;
+  do {
+    c = getchar();
+  } while (c == '\n' || c == '\r' || c == ' ');
+
+  char negative = c == '-';
+
+  for (; (c > 47 && c < 58); c = getchar())
+    *number = *number * 10 + c - 48;
+  if (negative) {
+    *number *= -1;
+  }
+}
+
+void next_unsigned(uint_t *number) {
+  register int_t c;
+  *number = 0;
+  do {
+    c = getchar();
+  } while (c == '\n' || c == '\r' || c == ' ');
+  for (; (c > 47 && c < 58); c = getchar())
+    *number = *number * 10 + c - 48;
+}
+
+void next_lower_letter(char *c) {
+  do {
+    *c = getchar();
+  } while (*c < 97 || *c > 122);
+}
+
+void next_upper_letter(char *c) {
+  do {
+    *c = getchar();
+  } while (*c < 65 || *c > 90);
+}
+
+int main(void) {
+  uint_t T, N, X, Y;
+  register uint_t S;
+  next_unsigned(&T);
+  while (T--) {
+    next_unsigned(&N);
+    next_unsigned(&X);
+    S = 0;
+    while (--N) {
+      next_unsigned(&Y);
+      S += (X > Y ? X - Y : Y - X) - 1; // abs(X-Y)-1
+      X = Y;
+    }
+    printf("%lu\n", S);
+  }
+}
